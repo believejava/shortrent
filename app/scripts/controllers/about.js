b31d1222ca6d67a24eaaa4632bc5a82485d0bee3
黄玮鹏 <chinawphuang@163.com>
2016-07-20 09:08:19 -0400

    login static html

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shortrentApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the shortrentApp
+ */
+angular.module('shortrentApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shortrentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shortrentApp
+ */
+angular.module('shortrentApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
